1a4772551541c4fafa1591de2c49802ea9395b73
moham <mohaemdattar8300@gmail.com>
2023-08-24 10:18:04 +0300

    * Finish working on the main page.

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,14 +8,122 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final TextEditingController _name = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text("Dialog"),
+        title: const Text("Home page"),
+        centerTitle: true,
       ),
-      body: const Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 12.0,),
+            TextFormField(
+              controller: _name,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    width: 3, //<-- SEE HERE
+                    color: Colors.blueAccent,
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                hintText: "Enter your name",
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    width: 3, //<-- SEE HERE
+                    color: Colors.blueAccent,
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+              ),
+              onFieldSubmitted: (String value) {
+                setState(() {
+                  _name.text = value;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Spacer(),
+            Column(
+              children: [
+                Text(
+                  _name.text == "" ? "No value yet" : _name.text,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    // side: BorderSide(color: Colors.yellow, width: 5),
+                    textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontStyle: FontStyle.normal),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _name.text = "";
+                    });
+                  },
+                  child: const Text(
+                    "Clear Text",
+                  ),
+                ),
+                ButtonBar(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        // side: BorderSide(color: Colors.yellow, width: 5),
+                        textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                      ),
+                      child: const Text("Animation Screen"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        // side: BorderSide(color: Colors.yellow, width: 5),
+                        textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                      ),
+                      child: const Text("Pokemon Screen"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
